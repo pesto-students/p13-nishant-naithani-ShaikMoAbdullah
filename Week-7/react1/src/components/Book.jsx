cfa04899f36be73719e780ae9d0d13d0f5ee51bd
Shaik Mohammad Abdullah <abdullah.shaik@zopsmart.com>
2024-01-15 08:50:40 +0530

    feat: add assignment 3

--- Week-7/react1/src/components/Book.jsx
+++ Week-7/react1/src/components/Book.jsx
@@ -1,12 +1,23 @@
 import "../styles/book.css";
-import { memo } from "react";
+import { memo, useState } from "react";
 
 function Book({ title, author, year }) {
+  const [show, setShow] = useState(true);
   return (
     <div className="book">
-      <h3>{title}</h3>
-      <p>Author: {author}</p>
-      <p>Year: {year}</p>
+      {show && (
+        <>
+          <h3>{title}</h3>
+          <p>Author: {author}</p>
+          <p>Year: {year}</p>
+        </>
+      )}
+      <button
+        className="border-2 border-black rounded p-1"
+        onClick={() => setShow(!show)}
+      >
+        Toggle
+      </button>
     </div>
   );
 }
